9e9ccca014ef970b888b6efbb3eca5ebcc7539a2
jinified <jinified@gmail.com>
2023-05-01 15:43:57 +0800

    Reset everything

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -24,6 +24,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: TextField(
+            decoration:
+                InputDecoration(hintText: "e.g. Aaron, Kadabra, Gengar")));
+    return Scaffold(
         backgroundColor: Colors.white60,
         body: Center(
             child: FractionallySizedBox(
@@ -31,42 +35,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: DecoratedBox(
                     decoration: BoxDecoration(
                         border: Border.all(color: Colors.red, width: 2)),
-                    child: Column(children: <Widget>[
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: 'Enter a search term',
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 12.0),
-                      Expanded(
-                          child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 24.0),
-                              child: CustomScrollView(slivers: <Widget>[
-                                SliverGrid(
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                          crossAxisCount: 3,
-                                          crossAxisSpacing: 12.0,
-                                          mainAxisSpacing: 12.0),
-                                  delegate: SliverChildBuilderDelegate(
-                                    (BuildContext context, int index) {
-                                      return Container(
-                                        alignment: Alignment.center,
-                                        color: Colors.teal[100 * (index % 9)],
-                                        child: Text('Grid Item $index'),
-                                      );
-                                    },
-                                    childCount: 20,
-                                  ),
-                                )
-                              ])))
-                    ])))));
+                    child: Column(
+                        children: const <Widget>[SizedBox(height: 12.0)])))));
   }
 
   void fetchContent() {
