31fb609da66a854561eb7d493963882f60be746a
jinified <jinified@gmail.com>
2023-07-15 17:56:38 +0800

    Completed animation of match

--- lib/tamago_start_match.dart
+++ lib/tamago_start_match.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:keranamu/widgets/tamago_card.dart';
+import 'package:collection/collection.dart';
 
 class TamagoStartMatchPage extends StatefulWidget {
   List<String> myHand;
@@ -17,8 +18,13 @@ class TamagoStartMatchPage extends StatefulWidget {
 
 class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
     with TickerProviderStateMixin {
+  String battleText = "Let's battle ⚔️";
   int animateHandIdx = 1;
   int battleIdx = -1;
+  List<int> oppScores = [];
+  List<int> myScores = [];
+  int myScore = 0;
+  int oppScore = 0;
   final double maxOffset = 400;
   late AnimationController _controller;
   late AnimationController _cardFlipController;
@@ -37,6 +43,15 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
       'https://cdn-icons-png.flaticon.com/512/3524/3524344.png',
       height: 200,
       width: 200);
+
+  Map<String, String> imagePathMap = {
+    'scissors': 'https://cdn-icons-png.flaticon.com/512/68/68847.png',
+    'rock':
+        'https://static.vecteezy.com/system/resources/thumbnails/002/103/779/small/clenched-fist-black-glyph-icon-vector.jpg',
+    'paper': 'https://cdn-icons-png.flaticon.com/512/142/142175.png',
+    'dragon': 'https://cdn-icons-png.flaticon.com/512/2119/2119228.png',
+    'rabbit': 'https://cdn-icons-png.flaticon.com/512/802/802338.png',
+  };
 
   Map<String, Image> imageMap = {
     'scissors': Image.network(
@@ -69,15 +84,29 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
     'rabbit': 1
   };
 
+  Map<String, int> cardTypeToIdx = {
+    'scissors': 0,
+    'rock': 1,
+    'paper': 2,
+    'dragon': 3,
+    'rabbit': 4
+  };
+
   List<String> draft = ["", "", "", "", ""];
 
   @override
   void initState() {
     super.initState();
+    for (final pair in IterableZip([widget.myHand, widget.oppHand])) {
+      int myHandIdx = cardTypeToIdx[pair[0]]!;
+      int oppHandIdx = cardTypeToIdx[pair[1]]!;
+      myScores.add(cardBattleScores[myHandIdx][oppHandIdx]);
+      oppScores.add(cardBattleScores[oppHandIdx][myHandIdx]);
+    }
     _controller = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 1000));
     _cardFlipController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 3000));
+        vsync: this, duration: const Duration(milliseconds: 1500));
     cardAnimation = Tween(begin: 0.0, end: 1.0).animate(_cardFlipController);
     animation = Tween(begin: 0.0, end: maxOffset).animate(_controller);
     _cardFlipController.addListener(() {
@@ -86,15 +115,28 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
       });
     });
     _cardFlipController.addStatusListener((status) {
-      if (status != AnimationStatus.completed) {
-        print("Flippening: ${DateTime.now().toString()}");
-      }
+      if (status != AnimationStatus.completed) {}
       if (status == AnimationStatus.completed &&
           battleIdx < widget.myHand.length) {
-        Future.delayed(const Duration(milliseconds: 500), () {
-          _cardFlipController.forward(from: 0.0);
-          battleIdx++;
+        setState(() {
+          myScore += myScores[battleIdx];
+          oppScore += oppScores[battleIdx];
         });
+        if (battleIdx == widget.myHand.length - 1) {
+          String gameOverText = "It's a draw";
+          if (myScore != oppScore) {
+            gameOverText = "You ${myScore > oppScore ? 'win 🎉' : 'lose ☹️'}";
+          }
+          setState(() {
+            battleText = gameOverText;
+            battleIdx++;
+          });
+        } else {
+          Future.delayed(const Duration(milliseconds: 500), () {
+            _cardFlipController.forward(from: 0.0);
+            battleIdx++;
+          });
+        }
       }
     });
     _controller.addStatusListener((status) {
@@ -158,6 +200,19 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
     };
   }
 
+  Image getImage(String myCard, String oppCard, int battleIdx, int cardIdx) {
+    return Image.network(imagePathMap[myCard]!,
+        height: 200,
+        width: 200,
+        opacity: cardIdx < battleIdx &&
+                cardBattleScores[cardTypeToIdx[myCard]!]
+                        [cardTypeToIdx[oppCard]!] <=
+                    cardBattleScores[cardTypeToIdx[oppCard]!]
+                        [cardTypeToIdx[myCard]!]
+            ? const AlwaysStoppedAnimation(0.4)
+            : const AlwaysStoppedAnimation(1.0));
+  }
+
   double calculateOffset(
       int idx, int animateHandIdx, double animationVal, double maxOffset) {
     double offset = maxOffset * idx;
@@ -185,9 +240,9 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
                   padding: const EdgeInsets.all(12),
                   child: Column(
                     children: [
-                      const Text(
-                        'Opponent',
-                        style: TextStyle(
+                      Text(
+                        'Opponent: $oppScore',
+                        style: const TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 32),
                       ),
                       const SizedBox(
@@ -222,12 +277,18 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
                                               rotateAngle: battleIdx == idx
                                                   ? rotateAngle
                                                   : 0,
-                                              icon:
-                                                  imageMap[e] ?? defaultImage));
+                                              icon: getImage(
+                                                  widget.oppHand[idx],
+                                                  widget.myHand[idx],
+                                                  battleIdx,
+                                                  idx)));
                                     }).toList());
                                   })))
                     ],
                   ))),
+          Text(battleText,
+              style:
+                  const TextStyle(fontWeight: FontWeight.w600, fontSize: 64)),
           Container(
               color: const Color(0xFFFDCEDF),
               width: MediaQuery.of(context).size.width * 0.8,
@@ -236,9 +297,9 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
                   padding: const EdgeInsets.all(12),
                   child: Column(
                     children: [
-                      const Text(
-                        'Me',
-                        style: TextStyle(
+                      Text(
+                        'Me: $myScore',
+                        style: const TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 32),
                       ),
                       const SizedBox(
@@ -273,8 +334,11 @@ class _TamagoStartMatchPageState extends State<TamagoStartMatchPage>
                                               rotateAngle: battleIdx == idx
                                                   ? rotateAngle
                                                   : 0,
-                                              icon:
-                                                  imageMap[e] ?? defaultImage));
+                                              icon: getImage(
+                                                  widget.myHand[idx],
+                                                  widget.oppHand[idx],
+                                                  battleIdx,
+                                                  idx)));
                                     }).toList());
                                   })))
                     ],
